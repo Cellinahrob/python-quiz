0a3bf3364d032465ecd357884e0d4edda12d3334
Cellinah <Celinarob@gmail.com>
2019-05-02 09:05:03 -0700

    add quiz

---
+++ shapes.py
@@ -0,0 +1,65 @@
+class Circle():
+    def __init__(self, r):
+        self.radius = r
+
+    def area(self):
+        return self.radius=πr2
+    
+    def circumfrence(self):
+        return self.radius=2πr
+
+Circle = Circle()
+print(Circle.area())
+print(Circle.circumfrence())
+
+
+class Squre():
+	def __init__(self, a):
+        self.side = a
+
+    def area(self):
+        return self.side=A=a2
+    
+    def perimeter(self):
+        return self.side=P=4a
+
+Squre = Circle()
+print(Squre.area())
+print(Squre.perimeter())
+
+
+class Rectangle():
+	 def __init__(self, W,l):
+        self.sides = W,l
+
+    def area(self):
+        return self.sides=A=wl
+    
+    def perimeter(self):
+        return self.sides=P=2(l+w)
+
+Rectangle = Circle()
+print(Rectangle.area())
+print(Rectangle.perimeter())
+
+
+
+class Sphere():
+	 def __init__(self,r):
+        self.radius = r
+
+    def Sarea(self):
+        return self.radius=A=4πr2
+    
+    def Volume(self):
+        return self.radius=V = 4/3(πr3)
+
+Sphere = Circle()
+print(Sphere.Sarea())
+print(Sphere.Volume())
+
+
+
+
+
+	
